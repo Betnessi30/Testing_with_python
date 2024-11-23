fbfc404be8a53517fb357e8a334910ef4d163d35
Grace <grace.betnessi@gmail.com>
2024-11-23 09:02:00 +0100

    tutorial report

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 # app.py
 from flask import Flask, jsonify
 
-app = Flask(__name__)
+app = Flask(__name__) ;
 
 
-@app.route("/project")
+@app.route('/')
 def home():
     return jsonify(message="Hello level 400 FET, Quality Assurance!")
 
